693f11925c5f612c60ae77826d45cad6010b0830
”aliaksandr.batskalevich” <aliaksandr.batskalevich@gmail.com>
2025-03-02 09:40:54 +0300

    Added FLW to white-list

--- white-list.js
+++ white-list.js
@@ -1,5 +1,6 @@
 export const whiteList = [
   786052803, // Aliaksandr Batskalevich
   436321559, // Maryia Batskalevich
+  5166905267, // Maryia Batskalevich FLW
   819081491, // Nikolaj Schitljak
 ];
